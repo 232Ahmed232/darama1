7786322913cf382cba9582b80697acea63b3d285
232Ahmed232 <x99aliahmed@gmail.com>
2025-09-14 17:54:55 +0500

    add tailwind

---
+++ src/componenets/SignUp/SignUp.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div className='text-center'>
+        <h1 className="text-3xl font-bold  my-4">
+        SignUp
+    </h1>
+    
+    </div>
+  )
+}
+
+export default SignUp
